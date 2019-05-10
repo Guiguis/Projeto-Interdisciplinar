1c33112a63cce89009ab1c25a4f9db63a74186eb
Pedro Quessada <pedro.quessada@saojudas.br>
2019-05-10 05:45:48 -0300

    Listar somente os grupos em que o professor logado participa em declaração

--- Projeto Interdisciplinar/src/controller/ListarGrupoDeclaracao.java
+++ Projeto Interdisciplinar/src/controller/ListarGrupoDeclaracao.java
@@ -12,7 +12,10 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import model.Grupo;
+import model.Professor;
+import service.AvaliacaoService;
 import service.GrupoService;
+import service.ProfessorBancaService;
 
 /**
  * Servlet implementation class ListarGrupoDeclaracao
@@ -39,7 +42,7 @@ public class ListarGrupoDeclaracao extends HttpServlet {
 
 		//BUSCA GRUPO DE ACORDO COM TURMA
 		GrupoService gs = new GrupoService();
-		ArrayList<Grupo> lstGrupo = null;
+		ArrayList<Grupo> lstGrupos = null;
 				
 		//Pega as informacoes principais da pagina
 		String acao = request.getParameter("acao");
@@ -47,8 +50,20 @@ public class ListarGrupoDeclaracao extends HttpServlet {
 		if(acao.equals("reiniciar")) {
 			int idTurma = (turmaIdSession != null) ? Integer.parseInt(turmaIdSession) : -1;
 			
-			lstGrupo = gs.loadGrupoByTurma(idTurma);
-			request.setAttribute("lstGrupo", lstGrupo);
+			lstGrupos = gs.loadGrupoByTurma(idTurma);
+				
+			//Verifica se o professor logado é administrador
+			Professor orientador = (Professor) session.getAttribute("usuario");
+			if(orientador.getAdministrador() == 0) {
+				//Verifica quais grupos o professor participou da banca
+				ProfessorBancaService pbs = new ProfessorBancaService();
+				Professor professorLogado = (Professor) session.getAttribute("usuario");
+				ArrayList<Grupo> lstGruposParticipados = pbs.gruposParticipados(lstGrupos, professorLogado);
+				
+				request.setAttribute("lstGrupo", lstGruposParticipados);
+			}
+			
+			request.setAttribute("lstGrupo", lstGrupos);
 		}
 		
 		RequestDispatcher dispatcher = request.getRequestDispatcher("ListarGruposDeclaracao.jsp");
--- Projeto Interdisciplinar/src/service/ProfessorBancaService.java
+++ Projeto Interdisciplinar/src/service/ProfessorBancaService.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import dao.ProfessorBancaDAO;
 import model.Aluno;
 import model.Banca;
+import model.Grupo;
 import model.Professor;
 import model.ProfessorBanca;
 
@@ -57,5 +58,37 @@ public class ProfessorBancaService implements Serializable{
 	public ArrayList<Professor> loadProfessoresBanca(int idBanca) {
 		return dao.loadProfessoresBanca(idBanca);
 	}
+	
+	//Retorna uma lista com os grupos que o usuario logado participou da banca
+	
+	public ArrayList<Grupo> gruposParticipados(ArrayList<Grupo> lstGrupos, Professor professorLogado){
+		ArrayList<Grupo> listaGrupoParticipados = new ArrayList<>();
+		
+		for(int i = 0; i < lstGrupos.size(); i++) {
+			
+			//Verifica se o professor é o orientador do grupo
+			if(lstGrupos.get(i).getOrientador().getId() == professorLogado.getId()) {
+				listaGrupoParticipados.add(lstGrupos.get(i));
+			}
+			else {
+				int idGrupo = lstGrupos.get(i).getId();
+				ProfessorBancaService pbs = new ProfessorBancaService();
+				Banca banca = pbs.loadBancaGrupo(idGrupo);
+				ArrayList<Professor> listaProfessores = pbs.loadProfessoresBanca(banca.getId());
+				
+				//percore a lista de professores que participaram da banca e verifica se um deles é o professor logadp
+				for(int j = 0; j < listaProfessores.size(); j++) {
+					if(listaProfessores.get(j).getId() == professorLogado.getId()) {
+						listaGrupoParticipados.add(lstGrupos.get(i));
+					}
+				}
+				
+			}
+		}
+		
+		
+		return listaGrupoParticipados;
+	}
+	
 	
 }
