ce74473317e39964ca0f760cf28830864007cb60
818149040 <818149040@MOOLIi0919.animaedu.intranet>
2019-05-14 20:31:14 -0300

    Adicionando LogFilter

---
+++ Projeto Interdisciplinar/src/Filter/LogFilter.java
@@ -0,0 +1,70 @@
+package Filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import model.Usuario;
+
+/**
+ * Servlet Filter implementation class LogFilter
+ */
+@WebFilter("/LogFilter")
+public class LogFilter implements Filter {
+
+    /**
+     * Default constructor. 
+     */
+    public LogFilter() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		// place your code here
+		request.setCharacterEncoding("UTF-8");
+		response.setCharacterEncoding("UTF-8");
+		HttpServletRequest req = (HttpServletRequest)request;
+		HttpSession session = req.getSession();
+		Usuario usuario = (Usuario)session.getAttribute("usuario");
+		String uri = req.getRequestURI();
+		
+		if(usuario == null){
+			System.out.println(uri);
+		} else {
+			System.out.println(usuario.getNome()+ " -> " + uri);
+		}
+		// pass the request along the filter chain
+		chain.doFilter(request, response);
+		if(usuario == null){
+			System.out.println(uri);
+		} else {
+			System.out.println(uri+ " -> " + usuario.getNome());
+		}
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
